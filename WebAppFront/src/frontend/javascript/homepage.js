18bc2bd62f6353a995ce5f9917b741f8bf96d3a0
daavanoosten <d.a.a.vanoosten@student.utwente.nl>
2023-06-16 17:27:55 +0200

    change credentials when clicked on account and footer is not necessary

---
+++ WebAppFront/src/frontend/javascript/homepage.js
@@ -0,0 +1,19 @@
+function showDialog() {
+    document.getElementById("accountPopUp").style.visibility = "visible";
+}
+
+function closeDialog() {
+    document.getElementById("accountPopUp").style.visibility = "hidden";
+}
+
+// function showUsernameInput(){
+//     document.getElementById("updateUsername").style.visibility = "visible";
+//     document.getElementById("accountPopUp").style.visibility = "visible";
+// }
+//
+// function showPasswordInput() {
+//     document.getElementById("updatePassword").style.visibility = "visible";
+
+
+
+
