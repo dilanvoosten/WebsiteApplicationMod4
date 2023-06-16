c326390d9afcb6b5083dea7c4f33d4df827f0ccf
daavanoosten <d.a.a.vanoosten@student.utwente.nl>
2023-06-17 00:19:52 +0200

    Created form for making new article, but can only add ONE section for now

--- WebAppFront/src/frontend/javascript/addArticle.js
+++ WebAppFront/src/frontend/javascript/addArticle.js
@@ -5,3 +5,12 @@ function showDialog() {
 function closeDialog() {
     document.getElementById("accountPopUp").style.visibility = "hidden";
 }
+
+// TODO create multiple sections, now it overrides the already created one
+function generateSection() {
+    document.getElementById("newSections").innerHTML =
+        `<label for="articleHeader"></label>
+        <input type="text" id="articleHeader" placeholder="Name your header here">
+        <label for="articleSection"></label>
+        <textarea  id="articleSection"  name="articleSection" placeholder="Write your text here"></textarea>`;
+}
