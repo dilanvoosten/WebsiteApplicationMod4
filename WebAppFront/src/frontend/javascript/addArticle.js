830d70c229b0e93e5e39b211472fe38442847591
daavanoosten <d.a.a.vanoosten@student.utwente.nl>
2023-06-16 17:35:31 +0200

    let account dialog show in article page

---
+++ WebAppFront/src/frontend/javascript/addArticle.js
@@ -0,0 +1,7 @@
+function showDialog() {
+    document.getElementById("accountPopUp").style.visibility = "visible";
+}
+
+function closeDialog() {
+    document.getElementById("accountPopUp").style.visibility = "hidden";
+}
