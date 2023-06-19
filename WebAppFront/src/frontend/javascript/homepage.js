1d8397079a433cf9357804338b5f1f292448d21b
daavanoosten <d.a.a.vanoosten@student.utwente.nl>
2023-06-19 21:47:34 +0200

    redesigned accountPopUp, now has new colors on different buttons and confirmation of new password

--- WebAppFront/src/frontend/javascript/homepage.js
+++ WebAppFront/src/frontend/javascript/homepage.js
@@ -6,13 +6,7 @@ function closeDialog() {
     document.getElementById("accountPopUp").style.visibility = "hidden";
 }
 
-// function showUsernameInput(){
-//     document.getElementById("updateUsername").style.visibility = "visible";
-//     document.getElementById("accountPopUp").style.visibility = "visible";
-// }
-//
-// function showPasswordInput() {
-//     document.getElementById("updatePassword").style.visibility = "visible";
+
 
 
 
